3fab61ffc4e6c567a0d854d2c217c939786c5081
姜佳龙 <jiang.jl@topsports.com.cn>
2022-09-27 10:45:39 +0800

    搞图宝v2.0.0版本

--- js/popup.js
+++ js/popup.js
@@ -1,31 +1,89 @@
-let btn = document.getElementById('gao');
+let btn = document.getElementById('getImg');
 let status = 0;
 // 点击按钮
 btn.onclick = function (e) {
     if (status === 0) {
         status = 1;
-        updateStatus("loading", "搞他中,莫乱点!!!")
+        // updateStatus("loading", "搞他中,莫乱点!!!")
         doGao('开搞', (response) => {
             if (response && (response.zhu.length > 0 || response.xiang.length > 0)) {
                 var zhu = response.zhu;
                 var xiang = response.xiang;
-                var zip = new JSZip();
-                var imgUrls = [...zhu, ...xiang]
-                try {
-                    saveToZip(zip, imgUrls, 0)
-                } catch (e) {
-                    status = 0;
-                    updateStatus("error", "完了,搞错了!!!");
+                let _html = "";
+                _html += `<h1>主图</h1>`
+                for (let i = 0; i < zhu.length; i++) {
+                    _html += `
+                    <div><img src="${zhu[i].src}"/><span>${zhu[i].name}</span></div>
+                    `
                 }
+                _html += `<h1>详情图</h1>`
+                for (let i = 0; i < xiang.length; i++) {
+                    _html += `
+                    <div><img src="${xiang[i].src}"/><span>${xiang[i].name}</span></div>
+                    `
+                }
+                document.getElementById("main").innerHTML = _html;
+
+                // 天际选中事件
+                var items = document.getElementById('main').getElementsByTagName('div');
+                for (let i = 0; i < items.length; i++) {
+                    items[i].onclick = function () {
+                        console.log(123)
+                        if (this.classList.contains('activation')) {
+                            //表示含有'checked'这个类名
+                            this.classList.remove('activation')
+                        } else {
+                            this.classList.add('activation')
+                        }
+                    }
+                }
+                status = 0;
             } else {
                 status = 0;
-                updateStatus("biegao", "页面不对吧\n别瞎搞!!!");
+                // updateStatus("biegao", "页面不对吧\n别瞎搞!!!");
             }
         });
     } else {
-        updateStatus("loading", "在搞呢,莫急!!!");
+        // updateStatus("loading", "在搞呢,莫急!!!");
     }
 }
+
+// 全选
+document.getElementById('allSelect').onclick = function () {
+    var items = document.getElementById('main').getElementsByTagName('div');
+    var flag = false;
+    for (let i = 0; i < items.length; i++) {
+        if (!items[i].classList.contains('activation')) {
+            flag = true;
+            break;
+        }
+    }
+    for (let i = 0; i < items.length; i++) {
+        if (flag) {
+            items[i].classList.add('activation');
+        } else {
+            items[i].classList.remove('activation');
+        }
+    }
+};
+
+let btn2 = document.getElementById('download');
+btn2.onclick = function () {
+    if (status === 0) {
+        var main = document.getElementById("main").getElementsByClassName("activation");
+        var imgUrls = []
+        for (let i = 0; i < main.length; i++) {
+            imgUrls.push({src: main[i].getElementsByTagName('img')[0].src, name: main[i].getElementsByTagName('span')[0].innerText})
+        }
+        var zip = new JSZip();
+        try {
+            saveToZip(zip, imgUrls, 0)
+        } catch (e) {
+            status = 0;
+            // updateStatus("error", "完了,搞错了!!!");
+        }
+    }
+};
 
 function saveToZip(zip, imgUrls, index) {
     getResources(index + 1, imgUrls[index].src, (data) => {
@@ -40,7 +98,7 @@ function saveToZip(zip, imgUrls, index) {
             zip.generateAsync({type: "blob"}).then(function (content) {
                 saveAs(content, "搞.zip");
                 status = 0;
-                updateStatus("end", "搞完了,还搞吗?");
+                // updateStatus("end", "搞完了,还搞吗?");
             });
         } else {
             saveToZip(zip, imgUrls, index);
