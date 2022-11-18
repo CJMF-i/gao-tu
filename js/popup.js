99e71f8a3bf664b7e0bfb28e82f3f036fd49b16c
姜佳龙 <jiang.jl@topsports.com.cn>
2022-11-18 15:04:37 +0800

    搞图宝助手优化

--- js/popup.js
+++ js/popup.js
@@ -1,6 +1,4 @@
 var BasePath = "./live2";
-let kaishi = [];
-let jieshu = [];
 
 let btn = document.getElementById('getImg');
 let status = 0;
@@ -8,7 +6,6 @@ let status = 0;
 btn.onclick = function (e) {
     if (status === 0) {
         status = 1;
-        // updateStatus("loading", "搞他中,莫乱点!!!")
         doGao('开搞', (response) => {
             if (response && (response.zhu.length > 0 || response.xiang.length > 0)) {
                 var zhu = response.zhu;
@@ -28,27 +25,31 @@ btn.onclick = function (e) {
                 }
                 document.getElementById("main").innerHTML = _html;
 
-                // 天际选中事件
+                // 添加选中事件
                 var items = document.getElementById('main').getElementsByTagName('div');
                 for (let i = 0; i < items.length; i++) {
                     items[i].onclick = function () {
-                        console.log(123)
                         if (this.classList.contains('activation')) {
                             //表示含有'checked'这个类名
                             this.classList.remove('activation')
+                            let currentSelect = document.getElementById('main').querySelectorAll(".activation")
+                            showMessage(`取消了<span>${this.querySelector("span").innerText}</span>，当前选中<span>${currentSelect.length}</span>张`, 5);
                         } else {
                             this.classList.add('activation')
+                            let currentSelect = document.getElementById('main').querySelectorAll(".activation")
+                            showMessage(`选中了<span>${this.querySelector("span").innerText}</span>，当前选中<span>${currentSelect.length}</span>张`, 5);
                         }
                     }
                 }
                 status = 0;
+                showMessage("页面数据读取完毕，快来选择喜欢的图片吧 ～～", 5);
             } else {
                 status = 0;
-                // updateStatus("biegao", "页面不对吧\n别瞎搞!!!");
+                showMessage("当前页面不对吧？目前只支持<span>京东、淘宝、天猫、coupang、1688、naver、gmarket、alibaba</span>，而且必须要在商品详情页面才可以哦 ～～");
             }
         });
     } else {
-        showMessage("在下载中哦，不要着急～～", 1000, 9);
+        showMessage("网页图片正在读取中哦，不要着急嘛 ～～", 3);
     }
 }
 
@@ -56,19 +57,20 @@ btn.onclick = function (e) {
 document.getElementById('allSelect').onclick = function () {
     var items = Array.from(document.getElementById('main').getElementsByTagName('div'));
     var flag = items.filter(item => item.classList.contains('activation'));
-    for (let i = 0; i < items.length; i++) {
-        if (!items[i].classList.contains('activation')) {
-            flag = true;
-            break;
-        }
+    if (items.length === 0) {
+        showMessage("还没有获取网页图片呢，快去获取一下吧 ～～", 5);
+        return;
     }
-    if (flag) {
-        showMessage("<span>全选/取消</span>按钮要全部选中才会触发取消，如果目前没有全部选中，需要先全选，然后取消哦", 1000, 9);
+
+    if (flag.length === 0) {
+        showMessage("全部选中了，快开始下载吧 ～～", 3);
+    } else if (flag.length < items.length) {
+        showMessage("<span>全选/取消</span>按钮要全部选中后，再次点击才可以触发全部取消哦 ～～");
     } else {
-        showMessage("已全部取消选中，快去挑选喜欢的图片吧", 1000, 9);
+        showMessage("已全部取消选中，快去挑选喜欢的图片吧 ～～", 4);
     }
     for (let i = 0; i < items.length; i++) {
-        if (flag) {
+        if (flag.length < items.length) {
             items[i].classList.add('activation');
         } else {
             items[i].classList.remove('activation');
@@ -84,13 +86,19 @@ btn2.onclick = function () {
         for (let i = 0; i < main.length; i++) {
             imgUrls.push({src: main[i].getElementsByTagName('img')[0].src, name: main[i].getElementsByTagName('span')[0].innerText})
         }
+        if (items.length === 0) {
+            showMessage("还没有获取网页图片呢，快去获取一下吧 ～～", 5);
+            return;
+        }
         var zip = new JSZip();
         try {
             saveToZip(zip, imgUrls, 0)
         } catch (e) {
             status = 0;
-            showMessage("哎呀，下载出错了～～", 2000, 9);
+            showMessage("哎呀，下载出错了，联系一下作者看一下吧，作者VX：<span>C473649383</span>", 5);
         }
+    } else {
+        showMessage("网页图片正在读取中哦，不要着急嘛 ～～", 3);
     }
 };
 
@@ -107,7 +115,7 @@ function saveToZip(zip, imgUrls, index) {
             zip.generateAsync({type: "blob"}).then(function (content) {
                 saveAs(content, "搞.zip");
                 status = 0;
-                showMessage("咯，给你，下载好了～～", 2000, 9);
+                showMessage("下载好了，快打开看一看吧 ～～", 4);
             });
         } else {
             saveToZip(zip, imgUrls, index);
--- live2/assets/waifu-tips.js
+++ live2/assets/waifu-tips.js
@@ -1,3 +1,23 @@
+let helps = [
+    "加<span>VX：C473649383</span> 可以进入交流群，获取最新更新消息哦",
+    "如果在日常使用过程中，有希望可以兼容的网站，欢迎联系 <br><span>VX：C473649383</span>",
+    "目前搞图宝已经支持了<span>京东、淘宝、天猫、coupang、1688、naver、gmarket、alibaba</span>",
+    "<span>搞图宝</span>是永久免费的，不要相信各种收费渠道哦 ～～",
+    "如果你有定制化的功能，也可以与我联系哦 ～～<br><span>VX：C473649383</span>",
+    "<span>搞图宝</span>正在茁壮成长中，过不了多久将会支持视频的导出哦 ～～",
+    "你相信光吗？你的优化与建议都是<span>搞图宝</span>的光 ～～",
+    "有没有觉得导出后压缩包名字怪怪的？我正在努力优化中，目前计划用商品名称做导出后的文件名，你有什么好的建议吗？欢迎叨扰 ～～",
+    "你想见见我的小姐妹吗？来交流群带你认识一下<span>萨菲亚、Kiro、lili、sagiri .....</span>"
+]
+
+let idle = [
+    "我说：“你是猪。”你说：“我是猪才怪!”从此我就叫你猪才怪。终于有一天，你忍不住当着众人的面对我吼道：“我不是猪才怪!”",
+    "护士看到一病人在病房喝酒，就走过去小声地对他说：“小心肝!” 病人微笑着说：“小宝贝。”",
+    "给我一秒钟，我会想起你，给我一分钟，我会牵挂你，给我一整天，我会思念你，给我一辈子，我会守护你，给我一条优化建议，我会满足你!",
+    "有一天，小明问他爸爸：“爸爸，我是不是傻孩子啊？” 爸爸说：“傻孩子，你怎么会是傻孩子呢？”",
+    "我的梦想是存一百万，现在我已经完成一半了，我存到了一百。",
+    "我从一无所有，到资产过亿，从家徒四壁，到豪车别墅，这些不是靠别人，完全是靠我自己，一点一滴， …………… ………….., 想出来的。",
+]
 
 function initWidget() {
     document.body.insertAdjacentHTML("beforeend", `<div id="waifu">
@@ -6,10 +26,13 @@ function initWidget() {
 		</div>`);
     setTimeout(() => {
         document.getElementById("waifu").style.bottom = 0;
-    }, 0);
+    }, 100);
 
     (function initModel() {
-        loadModel("", "你好，我是你的<span>搞图宝小秘</span>，有啥需要问我的，可以随时点我！");
+        loadModel("Kiana", "你好，我是你的<span>搞图宝助手 Kiana</span>，有不懂得可以随时点我哦 ～～");
+        document.getElementById("waifu").onclick = function () {
+            showMessage(helps)
+        };
     })();
 }
 
@@ -38,17 +61,19 @@ var modelListB = {
 var modelList = {
     "Kiana": BasePath + "/models/Kiana/model.json",
 }
-async function loadModel(modelName, message) {
+function loadModel(modelName, message) {
     if (modelName === "") {
         modelName = localStorage.getItem("modelName") === "" ? "Kiana" : localStorage.getItem("modelName");
     }
     localStorage.setItem("modelName", modelName);
-    showMessage(message, 4000, 10);
+    showMessage(message, 10);
     loadlive2d("live2d", modelList[modelName]);
 }
 
 var messageTimer;
+var topMessageTime;
 function showMessage(text, timeout) {
+    topMessageTime = new Date();
     if (messageTimer) {
         clearTimeout(messageTimer);
         messageTimer = null;
@@ -56,7 +81,7 @@ function showMessage(text, timeout) {
     // 如果传入的消息是数组，则随机选一条
     text = Array.isArray(text) ? text[Math.floor(Math.random() * text.length)] : text;
     // 如果如果没有传入消息显示最大时间，根据消息字数 * 200 毫秒
-    timeout = timeout === undefined ? text.length * 200 : timeout;
+    timeout = (timeout === undefined ? text.length * 300 : timeout * 1000);
     let tips = document.getElementById("waifu-tips");
     tips.innerHTML = text;
     tips.classList.add("waifu-tips-active");
@@ -65,3 +90,9 @@ function showMessage(text, timeout) {
         tips.classList.remove("waifu-tips-active");
     }, timeout);
 }
+
+setInterval(function () {
+    if (new Date() - topMessageTime > 7000) {
+        showMessage(idle, 8000);
+    }
+}, 10000);
